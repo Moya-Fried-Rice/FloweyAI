02e2b24e1fb19cf18930cc35e5447e8b3dbfdf3a
Moya-Fried-Rice <eemo2004@gmail.com>
2025-04-02 21:56:20 +0800

    Flowey

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function handleClick() {
-    const input = document.getElementById("userInput").value;
-    alert("You entered: " + input);
+  const input = document.getElementById("userInput").value.toLowerCase(); // Convert input to lowercase
+  const img = document.getElementById("image");
 
+  if (input === "bad") {
+      img.src = "assets/image/negative/1.png";
+  } else if (input === "good") {
+      img.src = "assets/image/positive/1.png";
+  } else if (input === "fine") {
+      img.src = "assets/image/neutral/1.png";
+  } else {
+      alert("You entered: " + input);
   }
+}
